f2f5f6e1591be1f43f543abfd51a7b6710a163fb
Ythalo Rossy Saldanha Lira <ythalorossy@gmail.com>
2015-10-27 03:31:16 +0000

    Added default port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res){
 
 app.use('/participantes', participantes);
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
